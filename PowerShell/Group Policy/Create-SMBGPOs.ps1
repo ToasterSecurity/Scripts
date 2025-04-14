c95f24037549489917b8b54ff5339d25605582cb
DeepestToaster <DeepestToaster@mijt.net>
2025-04-14 16:44:23 -0700

    Add SMB Encryption to SMBGPOs

--- PowerShell/Group Policy/Create-SMBGPOs.ps1
+++ PowerShell/Group Policy/Create-SMBGPOs.ps1
@@ -11,12 +11,14 @@ Start-Transcript -Path $DefaultLogLocation
 #  GPO  4 sets the registry to Require/Force SMB signing
 #  GPO  5 sets the registry to Disable SMB v1 Client
 #  GPO  6 sets the registry to Disable SMB v2 Client
-#  GPO  7 sets the registry to Enable SMB v1 as a rollback for if GPO 1 creates issues in the environment
-#  GPO  8 sets the registry to Enable SMB v2 as a rollback for if GPO 2 creates issues in the environment
-#  GPO  9 sets the registry to Disable SMB signing as a rollback for if GPO 3 creates issues in the environment
-#  GPO 10 sets the registry to Disable requiring/forcing SMB signing as a rollback for if GPO 4 creates issues in the environment
-#  GPO 11 sets the registry to Enable SMB v1 Client as a rollback for if GPO 6 creates issues in the environment
-#  GPO 12 sets the registry to Enable SMB v1 Client as a rollback for if GPO 6 creates issues in the environment
+#  GPO  7 sets the registry to Require SMB Encryption
+#  GPO  8 sets the registry to Enable SMB v1 as a rollback for if GPO 1 creates issues in the environment
+#  GPO  9 sets the registry to Enable SMB v2 as a rollback for if GPO 2 creates issues in the environment
+#  GPO 10 sets the registry to Disable SMB signing as a rollback for if GPO 3 creates issues in the environment
+#  GPO 11 sets the registry to Disable requiring/forcing SMB signing as a rollback for if GPO 4 creates issues in the environment
+#  GPO 12 sets the registry to Enable SMB v1 Client as a rollback for if GPO 5 creates issues in the environment
+#  GPO 13 sets the registry to Enable SMB v2 Client as a rollback for if GPO 6 creates issues in the environment
+#  GPO 14 sets the registry to Not Require SMB Encryption as a rollback for if GPO 7 Creates issues in the environment
 
 # Import the Group Policy Module
 Import-Module GroupPolicy
@@ -39,6 +41,9 @@ New-GPO -Name "PROD | Disable SMB v1 Client" -Comment "Sets the registry to Disa
 # Create GPO 6
 New-GPO -Name "PROD | Disable SMB v2 Client" -Comment "Sets the registry to Disable SMB v2 Client"
 
+# Create GPO 7
+New-GPO -Name "PROD | Require SMB Encryption" -Comment "Sets the registry to Require SMB Encryption"
+
 # Set GPO 1
 Set-GPPrefRegistryValue -Name "PROD | Disable SMB v1 Server" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\S\Parameters" -ValueName "SMB1" -Value 0 -Type "DWORD" -Order 1 -Action "Update"
 
@@ -57,13 +62,16 @@ Set-GPPrefRegistryValue -Name "PROD | Require SMB Signing" -Context Computer -Ke
 Set-GPPrefRegistryValue -Name "PROD | Require SMB Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" -Order 3 -Action "Update"
 Set-GPPrefRegistryValue -Name "PROD | Require SMB Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" -ValueName "RequireSecuritySignature" -Value 1 -Type "DWORD" -Order 4 -Action "Update"
 
-# Set GPO 6
+# Set GPO 5
 Set-GPPrefRegistryValue -Name "PROD | Disable SMB v1 Client" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\services\mrxsmb10" -ValueName "Start" -Value 4 -Type "DWORD" -Order 1 -Action "Update"
 Set-GPPrefRegistryValue -Name "PROD | Disable SMB v1 Client" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation" -ValueName "DependOnService" -Value "Bowser","MRxSmb20","NSI" -Type "MultiString" -Order 2 -Action "Update"
 
 # Set GPO 6
 Set-GPPrefRegistryValue -Name "PROD | Disable SMB v2 Client" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\services\mrxsmb20" -ValueName "Start" -Value 4 -Type "DWORD" -Order 1 -Action "Update"
 Set-GPPrefRegistryValue -Name "PROD | Disable SMB v2 Client" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation" -ValueName "DependOnService" -Value "Bowser","NSI" -Type "MultiString" -Order 2 -Action "Update"
+
+# Set GPO 7
+Set-GPPrefRegistryValue -Name "PROD | Require SMB Encryption" -Context Computer -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation" -ValueName "RequireEncryption" -Value 1 -Type "DWORD" -Order 1 -Action "Update"
 
 # Create Rollback GPO 1
 New-GPO -Name "Rollback | Disable SMB v1" -Comment "Sets the LAN Manager authentication level in the Computer Configuration"
@@ -78,10 +86,13 @@ New-GPO -Name "Rollback | Enable SMB Signing" -Comment "Disables SMB Signing via
 New-GPO -Name "Rollback | Require SMB Signing" -Comment "Removes requirement for SMB Signing via the registry"
 
 # Create Rollback GPO 5
-New-GPO -Name "Rollback | Disable SMB v1 Client" -Comment "Sets the registry to Disable SMB v1 Client"
+New-GPO -Name "Rollback | Disable SMB v1 Client" -Comment "Sets the registry to Enable SMB v1 Client"
 
 # Create Rollback GPO 6
-New-GPO -Name "Rollback | Disable SMB v2 Client" -Comment "Sets the registry to Disable SMB v1 Client"
+New-GPO -Name "Rollback | Disable SMB v2 Client" -Comment "Sets the registry to Enable SMB v2 Client"
+
+# Create Rollback GPO 7
+New-GPO -Name "Rollback | Require SMB Encryption" -Comment "Sets the registry to Not Require SMB Encryption"
 
 # Set Rollback GPO 1
 Set-GPPrefRegistryValue -Name "Rollback | Disable SMB v1" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -ValueName "SMB1" -Value 1 -Type "DWORD" -Order 1 -Action "Update"
@@ -108,6 +119,9 @@ Set-GPPrefRegistryValue -Name "Rollback | Disable SMB v1 Client" -Context Comput
 # Set Rollback GPO 6
 Set-GPPrefRegistryValue -Name "Rollback | Disable SMB v2 Client" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\services\mrxsmb20" -ValueName "Start" -Value 3 -Type "DWORD" -Order 1 -Action "Update"
 Set-GPPrefRegistryValue -Name "Rollback | Disable SMB v2 Client" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation" -ValueName "DependOnService" -Value "Bowser","MRxSmb20","NSI" -Type "MultiString" -Order 2 -Action "Update"
+
+# Set GPO 7
+Set-GPPrefRegistryValue -Name "Rollback | Require SMB Encryption" -Context Computer -Key "HKLM\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation" -ValueName "RequireEncryption" -Value 0 -Type "DWORD" -Order 1 -Action "Update"
 
 #Stop logging
 Stop-Transcript
