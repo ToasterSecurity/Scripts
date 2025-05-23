9930bc121aa072e3b777fa6b734c9f2beca2b928
DeepestToaster <DeepestToaster@mijt.net>
2025-05-22 21:27:05 -0700

    Added require ldap signing and fixed enable to actually only enable not require.

--- PowerShell/Group Policy/Create-LDAPGPOs.ps1
+++ PowerShell/Group Policy/Create-LDAPGPOs.ps1
@@ -8,7 +8,8 @@ Start-Transcript -Path $DefaultLogLocation
 
 # This creates ten Group Policy Objects (GPO's)
 #  GPO  1 sets the registry to Enable LDAP Signing
-#  GPO  2 sets the registry to Disable LDAP Signing 
+#  GPO  2 sets the registry to Require LDAP Signing
+#  GPO  3 sets the registry to Disable LDAP Signing 
 
 # Import the Group Policy Module
 Import-Module GroupPolicy
@@ -18,7 +19,14 @@ New-GPO -Name "PROD | Enable LDAP Signing" -Comment "Disables LLMNR via the regi
 
 # Set GPO 1
 Set-GPPrefRegistryValue -Name "PROD | Enable LDAP Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" -Order 1 -Action "Update"
-Set-GPPrefRegistryValue -Name "PROD | Enable LDAP Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" -ValueName "LDAPServerIntegrity" -Value 2 -Type "DWORD" -Order 1 -Action "Update"
+Set-GPPrefRegistryValue -Name "PROD | Enable LDAP Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" -ValueName "LDAPServerIntegrity" -Value 1 -Type "DWORD" -Order 1 -Action "Update"
+
+# Create GPO 2
+New-GPO -Name "PROD | Require LDAP Signing" -Comment "Disables LLMNR via the registry"
+
+# Set GPO 2
+Set-GPPrefRegistryValue -Name "PROD | Require LDAP Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" -Order 1 -Action "Update"
+Set-GPPrefRegistryValue -Name "PROD | Require LDAP Signing" -Context Computer -Key "HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Parameters" -ValueName "LDAPServerIntegrity" -Value 2 -Type "DWORD" -Order 1 -Action "Update"
 
 # Create Rollback GPO 2
 New-GPO -Name "Rollback | Enable LDAP Signing" -Comment "Enables LLMNR via the registry"
